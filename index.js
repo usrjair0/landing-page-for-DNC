156e95ad9e2a93269575e70ebdc7c8b79c0118c0
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-01-22 18:06:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var leftarrow = window.document.getElementById("leftarrow")
+var Leonardocard = window.document.getElementById("Leonardocard")
+var Samanthacard = window.document.getElementById("Samanthacard")
+var Brunacard = window.document.getElementById("Brunacard")
+var rightarrow = window.document.getElementById("rightarrow")
+/*aqui estou denominando as variáveis, estou dizendo que a var Leonardocard
+deve ser acessada pelo windows acessando o documentro e pegando o elemento
+do html pelo id*/
+
+function scrollright(){
+Leonardocard.style ="display:none"
+Brunacard.style ="display:flex"
+rightarrow.style ="display:none"
+leftarrow.style ="display:flex; margin-top:28px"
+}
+function scrollleft(){
+Leonardocard.style ="display:flex"
+Brunacard.style ="display:none"
+rightarrow.style ="display:flex; margin-top:28px"
+leftarrow.style ="display:none"
+}
