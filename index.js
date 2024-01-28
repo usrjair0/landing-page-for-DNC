89e7136085a7d7055292587fc8e13ea6748a4213
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-01-28 19:57:27 -0300

    final project

---
+++ index.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    effect: 'cube',
+  cubeEffect: {
+    slideShadows: false,
+  },
+    
+});
