df286997e0acd969bb5061048abe059d7a4ce9b6
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-01-28 18:21:34 -0300

    add some changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
